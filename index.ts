e0707696aace211b097b700fce4b47de3bde767f
Cesar Herrera <cesarherreras@live.com>
2022-09-16 13:24:57 -0500

    body-parser

--- index.ts
+++ index.ts
@@ -1,14 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 import routerApi from './routes/routes';
-import * as bodyParser from 'body-parser';
 import { dbConnection } from './db/mongoDB';
 import { config } from './config/config';
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.json());
 app.use(cors());
 dbConnection(config.dbUrl!);
 app.use('/', express.static('public'));
